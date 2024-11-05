21fa2d1c9ff7056366a2a0417d6f7f39638e1c25
Federico Hidalgo <fhidalgom1500@alumno.ipn.mx>
2024-11-04 19:13:34 -0600

    Se crea el proyecto 2, clonado del proyecto 1

---
+++ generadorAlgoritmos.py
@@ -0,0 +1,27 @@
+from generadorModelos import *
+
+def BFS(self, s):
+    """
+    Búsqueda a lo Ancho
+    Genera un Arbol a partir de un Grafo. Explora desde s 
+    en todas las direcciones posibles agregando nodos una capa a la ves
+    """
+    return s
+
+def dfsRecursiva(self, s):
+    """
+    Búsqueda en Profundidad
+    Genera un Arbol a partir de un Grafo. Recorriendo todos los
+    nodos desde sde manera ordenada pero no uniforme.
+    Se generta de forma recursiva.
+    """
+    return s
+
+def dfsIterativa(self, s):
+    """
+    Búsqueda en Profundidad
+    Genera un Arbol a partir de un Grafo. Recorriendo todos los
+    nodos desde sde manera ordenada pero no uniforme.
+    Se generta de forma iterativa.
+    """
+    return s 
